68e730b6acb8f830e296bd64c5d6dc80df488406
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-03-08 13:02:30 +0200

    done

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+
+function Button() {
+    const handleClickOnButton = () => {
+        
+    }
+    
+    return(
+        <button onClick={() => handleClickOnButton()}>Press me</button>
+    )
+    
+}
+export default Button
